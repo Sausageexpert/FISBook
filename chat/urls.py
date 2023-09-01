6ca17f4e7b00ec45531bd395794f1f9caf1636a1
Sausageexpert <68068569+Sausageexpert@users.noreply.github.com>
2023-09-01 06:47:42 +0530

    Delete chat directory

--- chat/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('<str:room>', views.room, name='room'),
-    path('', views.home, name='home'),
-    path('checkForRoom/', views.checkForRoom, name='checkForRoom')
-]
